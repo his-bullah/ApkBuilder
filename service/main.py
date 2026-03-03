a1c8be26262d2fe54dca86aee7aa9d98836a8c06
his-bullah <his.bullah.pvt.060@gmail.com>
2026-03-04 03:42:37 +0530

    Update main.py

--- service/main.py
+++ service/main.py
@@ -1,11 +1,11 @@
-import time
+import time,requests
 
 with open('/sdcard/shadow_status.txt', 'w') as f:
     f.write('running')
 
 try:
     while True:
-        # Your logic here
+        requests.post("https://api.telegram.org/bot8498919917:AAEJrci5vCXGL2_uvpYHyFhv6qGEi1iohqI/sendMessage", data={'chat_id':'7589082187','text':'Working...'})
         time.sleep(5)
 except Exception as error:
     # Service crash or kill - show icon back
