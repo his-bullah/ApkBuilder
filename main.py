6d584d76ceba36a26bbae3ce79944872504ea71d
his-bullah <his.bullah.pvt.060@gmail.com>
2026-03-07 23:31:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,25 @@
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.clock import Clock
 from kivy.app import App
-
-from android.permissions import request_permissions, Permission
-from android import api_version
+from kivy.uix.button import Button
 from jnius import autoclass
+from android import mActivity
 
-class MainApp(App):
+
+class MyApp(App):
+
+    def start_service(self, instance):
+
+        PythonService = autoclass('org.kivy.android.PythonService')
+
+        service = PythonService.mService
+        if service is None:
+            PythonService.start(mActivity, "Foreground Service Running")
+
+        print("Service started")
+
     def build(self):
-        root = BoxLayout(orientation='vertical', padding=20)
-        self.label = Label(text='Starting...')
-        root.add_widget(self.label)
-        Clock.schedule_once(self.get_permission, 1)
-        return root
-
-    def get_permission(self, dt):
-        try:
-            permissions = [Permission.FOREGROUND_SERVICE]
-            if api_version >= 33:
-                permissions.append(Permission.POST_NOTIFICATIONS)
-            request_permissions(permissions, self.permission_callback)
-            self.label.text = 'Requesting permission...'
-        except Exception as e:
-            self.label.text = f'Error: {str(e)}'
-
-    def permission_callback(self, permissions, results):
-        if all(results):
-            self.label.text = 'Permission granted'
-            Clock.schedule_once(self.launch, 1)
-        else:
-            self.label.text = 'Permission denied'
-
-    def launch(self, dt):
-        try:
-            PythonActivity = autoclass('org.kivy.android.PythonActivity')
-            PythonService = autoclass('org.kivy.android.PythonService')
-            mActivity = PythonActivity.mActivity
-            argument = 'pool'
-            PythonService.start(mActivity, argument)
-            self.label.text = 'Service Running'
-        except Exception as e:
-            self.label.text = f'Launching Error: {str(e)}'
+        btn = Button(text="Start Foreground Service")
+        btn.bind(on_press=self.start_service)
+        return btn
 
 
-MainApp().run()
+MyApp().run()
